1bca1cac05c3fa5ceed520f738654647a3d17537
jimfredferreras <jimfedmarifajardo.ferreras@bicol-u.edu.ph>
2024-05-05 23:18:07 +0800

    modified:   README.md 	modified:   android/app/build.gradle 	new file:   android/app/google-services.json 	modified:   android/settings.gradle 	new file:   firebase.json 	new file:   lib/firebase_options.dart 	modified:   lib/main.dart 	modified:   macos/Flutter/GeneratedPluginRegistrant.swift 	modified:   pubspec.lock 	modified:   pubspec.yaml 	modified:   windows/flutter/generated_plugin_registrant.cc 	modified:   windows/flutter/generated_plugins.cmake

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:bicol_folkdance/pages/welcome.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
   runApp(const MyApp());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -11,6 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: OpeningPage(),
     );
   }
